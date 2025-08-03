0f2782992a75d6a702af4795c49ab2c1c0c91d64
Erbolot <eraabc.main@gmail.com>
2025-08-03 22:13:45 +0600

    поставил иконки навигаций по смыслу

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from accounts.views import custom_login, RegisterView,ProfileView,ProfileListView
@@ -6,6 +7,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/',custom_login,name='login'),
     path('register/',RegisterView.as_view(),name='register'),
+    path('logout/',LogoutView.as_view(),name='logout'),
 
 
     path('profiles/',ProfileListView.as_view(),name='profiles'),
