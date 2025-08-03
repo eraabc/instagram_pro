f7cfbdfdea51679e5646dd13d0f4e28601911be0
Erbolot <eraabc.main@gmail.com>
2025-08-03 22:36:18 +0600

    реализовал удаление и редактирование поста

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from webapp.views import IndexView, CreatePostView
+from webapp.views import IndexView, CreatePostView,UpdatePostView,DeletePostView
 
 app_name = 'webapp'
 urlpatterns = [
     path('',IndexView.as_view(),name='index'),
     path('create_post/',CreatePostView.as_view(),name='create_post'),
+    path('post/<int:pk>/update/',UpdatePostView.as_view(),name='update_post'),
+    path('post/<int:pk>/delete/', DeletePostView.as_view(), name='delete_post'),
 ]
--- webapp/views/post.py
+++ webapp/views/post.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-from django.urls import reverse
-from django.views.generic import CreateView, ListView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
 from webapp.forms import PostForm, SearchForm
 from webapp.models import PostModel
@@ -24,3 +24,28 @@ class CreatePostView(LoginRequiredMixin,CreateView):
 
     def get_success_url(self):
         return reverse('webapp:index')
+
+
+class UpdatePostView(PermissionRequiredMixin,UpdateView):
+    template_name = 'post/update_post.html'
+    permission_required = 'webapp.change_post'
+    form_class = PostForm
+    model = PostModel
+
+    def has_permission(self):
+        return super().has_permission() or self.request.user == self.get_object().author
+
+    def get_success_url(self):
+        return reverse('webapp:index')
+
+
+class DeletePostView(PermissionRequiredMixin,DeleteView):
+    template_name = 'post/delete_post.html'
+    model = PostModel
+    success_url = reverse_lazy('webapp:index')
+
+    permission_required = 'webapp.delete_post'
+
+    def has_permission(self):
+        return super().has_permission() or self.request.user == self.get_object().author
+
