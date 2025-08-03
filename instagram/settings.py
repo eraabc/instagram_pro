075ad43d3f2a0057df8ea15200b0278a915a085d
Erbolot <eraabc.main@gmail.com>
2025-08-03 11:35:35 +0600

    создал приложение webapp

--- instagram/settings.py
+++ instagram/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'webapp',
 ]
 
 
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'instagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'accounts/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
